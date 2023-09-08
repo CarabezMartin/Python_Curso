b6125103c5a60d3b236fe967c82f5629a4ed2dfe
Hector <hector.camacho@iidea.com.mx>
2023-09-08 16:53:02 -0600

    hcamacho: Leccion 2 Variables

---
+++ Leccion_2/main.py
@@ -0,0 +1,10 @@
+#Leccion 2: Variables
+
+nombre = "Hector Camacho"
+telefono = 3326265492
+correo = "Test@test.com.mx"
+
+#Con id se accede el id de la memoria donde se encuentra almacenada la variable
+print(id(nombre),"-",nombre)
+print(id(telefono),"-",telefono)
+print(id(correo),"-",correo)
