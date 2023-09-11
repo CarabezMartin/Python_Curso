bfef337b7d3d6aca87b685948d8df4cc6171b96f
Hector <hector.camacho@iidea.com.mx>
2023-09-11 16:07:34 -0600

    hcamacho: renombramiento de carpetas y funcion input() en la leccion 4

---
+++ Leccion_4/main.py
@@ -0,0 +1,9 @@
+# Lección 4: Funcion input()
+
+calificacionDia = int(input("Como estuvo tu dia (1 al 10): "))
+print("Mi dia estuvo de: ", calificacionDia)
+
+tituloLiibro = input("Proporciona el titulo del libro: ")
+autorLibro = input("Proporciona el autor del libro: ")
+
+print("El libro " + tituloLiibro + " " + "fue escrito por " + autorLibro)
