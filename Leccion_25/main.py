502464edb17ed842d46086a309516dab81d10238
Hector <hector.camacho@iidea.com.mx>
2023-09-26 16:57:42 -0600

    hcamacho: Intentando implementar consultas parametrizadas

--- Leccion_25/main.py
+++ Leccion_25/main.py
@@ -11,10 +11,12 @@ try:
     conexion = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL server};SERVER='+server+';DATABASE='+bd+';UID='+user+';PWD='+password)
     print('Conexion exitosa')
 
-    query_users= 'SELECT TOP(10) * FROM Users'
+    query_users= 'SELECT * FROM Users WHERE UserID= %s'
 
     with conexion.cursor() as cursor:
-        cursor.execute(query_users)
+        id_user = input('Ingrese el Id de la persona: ')
+        print(id_user)
+        cursor.execute(query_users,(id_user,))
 
         res = cursor.fetchall()
 
